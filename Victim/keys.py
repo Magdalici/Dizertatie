31286a1ac4eba0d7121f0aa53f847fd9c2da4147
Maria-Magdalena <magda@laptop-magda>
2021-04-22 22:24:04 +0300

    Add required file for MISP auth

---
+++ Victim/keys.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+misp_url = 'https://misp.ecad.tuiasi.local:8443/'
+misp_key = 'IvXDgXKHU7TVbKInSbwgXkFW7yC36qXAK2bZQjJX' # The MISP auth key can be found on the MISP web interface under the automation section
+misp_verifycert = False
+misp_client_cert = ''
+proofpoint_key = 'Your Proofpoint TAP auth key'
